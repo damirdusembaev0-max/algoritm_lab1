7e60eaa5f6f7f422b549b7ca9ffe5dd400fb8567
damirdusembaev0-max <damirdusembaev0@gmail.com>
2026-03-16 15:26:31 +0500

    lab9

---
+++ lab9/task1-10.py
@@ -0,0 +1,4 @@
+class Node:
+    def __init__(self, data):
+        self.data = data  # Поле для хранения данных [cite: 29, 30]
+        self.next = None  # Ссылка на следующий элемент [cite: 29, 31]
